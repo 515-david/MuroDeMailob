ec279fe600992e7a0162a1091aa59a4d70ccc2bb
515-david <carvajalinofernandezjosedavid@gmail.com>
2020-10-12 20:28:25 -0500

    muro de mi amorcito

---
+++ js/scriptmuro.js
@@ -0,0 +1,9 @@
+
+
+$('.cambia-color').mouseover(function(){
+    $(this).css("background","black").css("color","white");
+});
+
+$('.cambia-color').mouseout(function(){
+    $(this).css("background","white").css("color","black");
+});
